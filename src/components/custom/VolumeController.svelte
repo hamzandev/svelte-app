932954716582a661c50cb202528b6fcb0cc2aafd
hamzandev <hamzankechil@gmail.com>
2024-08-08 21:38:57 +0800

    Topic: Reactive statement (volume control example)

---
+++ src/components/custom/VolumeController.svelte
@@ -0,0 +1,13 @@
+<script>
+  let volume = 0;
+
+  // Reactive statement example
+  $: if (volume == 15)
+    alert("Are you sure? High volume might damage your earphones!");
+</script>
+
+<div class="p-5 rounded-lg border-2 border-muted w-max text-center">
+  <h1 class="text-lg font-bold mb-3">Volume Controller</h1>
+  <p class="uppercase font-bold">Volume : {volume}</p>
+  <input type="range" max="20" bind:value={volume} />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,22 @@
 <script>
-import AlertDialog from "../components/custom/AlertDialog.svelte";
-import EditProfile from "../components/custom/EditProfile.svelte";
-import { Button } from "$lib/components/ui/button";
+  import AlertDialog from "../components/custom/AlertDialog.svelte";
+  import EditProfile from "../components/custom/EditProfile.svelte";
+  import {Button} from "$lib/components/ui/button";
+  import VolumeController from "../components/custom/VolumeController.svelte";
 </script>
 
 <svelte:head>
-    <title>Svelte + Shadcn UI</title>
-	<meta name="description" content="Built by Zanns" />
+  <title>Svelte + Shadcn UI</title>
+  <meta name="description" content="Built by Zanns" />
 </svelte:head>
 
 <div>
-    <h1>Homepage</h1>
+  <h1>Homepage</h1>
 
-    <AlertDialog/>
-    <span class="mx-3">VS</span>
-    <EditProfile/>
+  <AlertDialog />
+  <span class="mx-3">VS</span>
+  <EditProfile />
+
+  <br /><br />
+  <VolumeController />
 </div>
